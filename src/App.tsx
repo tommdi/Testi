46f44624184fcd9e4909e1782f329d93a411eda4
Arnkil <tommi.arnkil@cgi.com>
2022-11-11 12:45:25 +0200

    adding files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Home } from './components/Home';
+import { Signup } from './components/Singup';
+import { Login } from './components/Login';
+import { UserAuth } from './components/UserAuth'
+import { SignOut } from './components/SignOut';
+import { NavBar } from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Jwt } from './components/Jwt';
+import { Verify } from './components/Verify';
+
 
 function App() {
+  const [spinner, setSpinner] = useState("")
+  const [authName, setAuthName] = useState("")
+  const [jwtToken, setJwtToken] = useState("")
+
+  const getData = (data: any) => {
+    setSpinner("app");
+  }
+
+  const getUser = (data: any) => {
+        console.log("app.js", data)
+        setAuthName(data)
+  }
+
+  const getJWT = (data:any) => {
+    setJwtToken(data);
+  }
+  //<Kokeilu Tokeni={jwtToken}/>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <>
+          <NavBar UserName={authName}/>
+          
+          <Jwt JWT={getJWT} spinner={spinner}/>
+          
+          </>
+          <Routes>
+          <Route path='/' element={<Home UserName={authName} Tokeni={jwtToken}/>}/>
+          <Route path='Login' element={<Login onSubmit={getData}/>}/>
+          <Route path='Login/Register' element={<Signup/>}/>
+          </Routes>
+      <UserAuth spinner={spinner} user={getUser}/>
+      </div>
+      </Router>
     </div>
   );
 }
